0a71d82e4a7c1b8718e14faf637003f813d3d241
hungson2520 <hungson2520@gmail.com>
2023-08-19 13:55:28 +0700

    them 2 bai giai thuat

--- index.js
+++ index.js
@@ -1,60 +1,81 @@
 console.log("sua lan 2 vao luc 19/08/2023 10:40 AM");
-class GFG {
-  checkPalindrome(s) {
-    let n = s.length;
-    let i = 0,
-      j = n - 1;
-    while (i < j) {
-      if (s[i] !== s[j]) return false;
-      i++;
-      j--;
-    }
-    return true;
+// class GFG {
+//   checkPalindrome(s) {
+//     let n = s.length;
+//     let i = 0,
+//       j = n - 1;
+//     while (i < j) {
+//       if (s[i] !== s[j]) return false;
+//       i++;
+//       j--;
+//     }
+//     return true;
+//   }
+
+//   Partition(res, s, idx, curr) {
+//     if (idx === s.length) {
+//       res.push([...curr]);
+//       return;
+//     }
+//     console.log(
+//       "------------------------------------idx::--------------------------idx là",
+//       idx
+//     );
+
+//     let t = "";
+
+//     // s=geeks
+//     for (let i = idx; i < s.length; i++) {
+//       t += s[i];
+//       console.log("chuỗi t là", t);
+//       console.log("chuỗi curr là ", curr);
+//       console.log(" số lượng chuỗi res là ", res.length);
+//       console.log("chuỗi res là ", res);
+
+//       if (this.checkPalindrome(t)) {
+//         curr.push(t);
+
+//         this.Partition(res, s, i + 1, curr);
+
+//         curr.pop();
+//       }
+//     }
+//   }
+// }
+
+// let ob = new GFG();
+
+// let res = [];
+// let s = "MOMANDDAD";
+// let idx = 0;
+// let curr = [];
+
+// ob.Partition(res, s, idx, curr);
+
+// for (let v of res) {
+//   let output = "";
+//   for (let it of v) {
+//     output += it + " ";
+//   }
+//   console.log("output palindrome là:", output);
+// }
+//////-------------------------BÀI 2
+
+function checkPalindrome(str) {
+  // Calculating string length
+  const len = str.length;
+
+  // Traversing through the string
+  // upto half its length
+  for (let i = 0; i < len / 2; i++) {
+    // Comparing i th character
+    // from starting and len-i
+    // th character from end
+    if (str[i] != str[len - i - 1]) return false;
   }
 
-  Partition(res, s, idx, curr) {
-    if (idx === s.length) {
-      res.push([...curr]);
-      return;
-    }
-    console.log(
-      "------------------------------------idx::--------------------------idx là",
-      idx
-    );
-
-    let t = "";
-
-    // s=geeks
-    for (let i = idx; i < s.length; i++) {
-      t += s[i];
-      console.log("chuỗi t là", t);
-      console.log("chuỗi curr là ", curr);
-      console.log("chuỗi res là ", res);
-
-      if (this.checkPalindrome(t)) {
-        curr.push(t);
-
-        this.Partition(res, s, i + 1, curr);
-
-        curr.pop();
-      }
-    }
-  }
+  // If the above loop doesn't return then it is
+  // palindrome
+  return true;
 }
-
-let ob = new GFG();
-
-let res = [];
-let s = "LEHUNGSON";
-let idx = 0;
-let curr = [];
-
-ob.Partition(res, s, idx, curr);
-
-for (let v of res) {
-  let output = "";
-  for (let it of v) {
-    output += it + " ";
-  }
-  console.log("output palindrome là:", output);
-}
+console.log(`check ket qua 12322`, checkPalindrome("12322"));
